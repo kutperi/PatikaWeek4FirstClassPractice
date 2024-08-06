071d5bf83edaddcf85476542b9801779891bcb4c
Bilal <bilalbilin12@gmail.com>
2024-08-06 11:56:00 +0300

    Add project files.

---
+++ PatikaWeek4FirstClassPractice/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PatikaWeek4FirstClassPractice
+{
+    // Creating the class and setting properties
+    public class Person
+    {       
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public string Statue { get; set; }
+
+        public int BirthYear { get; set; }
+
+     
+        // Method to print informations
+        public void Introduce()
+        {
+            Console.WriteLine($"Merhaba benim adım {Name} soyadım {Surname} doğum tarihim {BirthYear} yılı ve bu kurumda statüm {Statue}dir.");
+        }
+        
+    }
+}
---
+++ PatikaWeek4FirstClassPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿// Öğrencilerin ve öğretmenlerin bilgilerini yönetmek için Person adında genel bir sınıf oluşturmanız isteniyor. Bu sınıfın özellikleri şunlar olmalı: Ad, Soyad, Doğum Tarihi.
+
+// Person adında bir sınıf oluşturun ve bu özellikleri içeren public property 'ler (get/set) tanımlayın.
+
+// Main metodu içinde, bu sınıftan birkaç öğrenci ve öğretmen nesnesi oluşturun ve bilgilerini konsola yazdırın. 
+
+
+using PatikaWeek4FirstClassPractice;
+
+// Creating the students and setting information about them.
+Person student1 = new Person();
+
+student1.Name = "Bilal";
+student1.Surname = "Bilin";
+student1.Statue = "öğrenci";
+student1.BirthYear = 1997;
+
+Person student2 = new Person();
+student2.Name = "Azadi";
+student2.Surname = "Kaya";
+student2.Statue = "öğrenci";
+student2.BirthYear = 1993;
+
+Person teacher1 = new Person();
+
+teacher1.Name = "İlhan Sami";
+teacher1.Surname = "Çomak";
+teacher1.Statue = "öğretmen";
+teacher1.BirthYear = 1973;
+
+// Calling the method to print informations.
+student1.Introduce();
+student2.Introduce();
+teacher1.Introduce();
